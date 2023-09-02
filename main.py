46829c43af3560f678a213b1c0ef39ff8b66b2e4
iiibrahimaslam423 <iiibrahimaslam@gmail.com>
2023-09-02 11:27:31 -0400

    Rename grime variables to include 'grime'

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This main file checks the hardwax scrape results and attempts to add them each to a spotify playlist.
 
-from hardwax import artist_title
+from hardwax import grime_artist_title
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from api_secrets import CLIENT_ID, CLIENT_SECRET
@@ -11,9 +11,9 @@ sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_sec
 
 json_data = []
 
-json_data.append(sp.search(artist_title[0], 1, 0, 'album', None))
+json_data.append(sp.search(grime_artist_title[0], 1, 0, 'album', None))
 
-# for project in artist_title:
+# for project in grime_artist_title:
 #     json_data.append(sp.search(project, 1, 0, 'album', None))
 
 print(json_data)
