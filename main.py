eebc2088b5c7d3e9f6477d20468d42fa602693b1
iiibrahimaslam423 <iiibrahimaslam@gmail.com>
2023-09-02 11:35:19 -0400

    Add variable and loop for house_json_data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This main file checks the hardwax scrape results and attempts to add them each to a spotify playlist.
 
-from hardwax import grime_artist_title
+from hardwax import grime_artist_title, house_artist_title
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from api_secrets import CLIENT_ID, CLIENT_SECRET
@@ -10,10 +10,10 @@ redirect_uri = 'https://www.google.com/'
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri))
 
 grime_json_data = []
+house_json_data = []
 
-grime_json_data.append(sp.search(grime_artist_title[0], 1, 0, 'album', None))
+for grime_project in grime_artist_title:
+    grime_json_data.append(sp.search(grime_project, 1, 0, 'album', None))
 
-# for project in grime_artist_title:
-#     grime_json_data.append(sp.search(project, 1, 0, 'album', None))
-
-print(grime_json_data)
+for house_project in house_artist_title:
+    house_json_data.append(sp.search(house_project, 1, 0, 'album', None))
