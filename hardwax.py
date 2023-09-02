4801147bf71a58f1b7baa3e266defcffa99098bd
iiibrahimaslam423 <iiibrahimaslam@gmail.com>
2023-09-01 21:23:40 -0400

    Remove class defintion, leaving basic code

--- hardwax.py
+++ hardwax.py
@@ -3,19 +3,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-class Hardwax:
+url = "https://hardwax.com/grime/?focus=only_downloads&page=1"
 
-    url = "https://hardwax.com/grime/?focus=only_downloads&page=1"
+request = requests.get(url)
 
-    request = requests.get(url)
+soup = BeautifulSoup(request.content, 'html.parser')
 
-    soup = BeautifulSoup(request.content, 'html.parser')
+h2_tags = soup.find_all('h2')
 
-    h2_tags = soup.find_all('h2')
+artist_title = []
 
-    artist_title = []
-
-    for tag in h2_tags:
+for tag in h2_tags:
         artist_title.append(tag.get_text())
 
-    artist_title = list(dict.fromkeys(artist_title))
+artist_title = list(dict.fromkeys(artist_title))
