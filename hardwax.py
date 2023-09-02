abcfca89eeef055a9f71540ab5be7e023bf39606
iiibrahimaslam423 <iiibrahimaslam@gmail.com>
2023-09-02 11:31:36 -0400

    Copy code to get house artist/title info

--- hardwax.py
+++ hardwax.py
@@ -21,3 +21,18 @@ for tag in grime_h2_tags:
 grime_artist_title = list(dict.fromkeys(grime_artist_title))
 
 # below is gathering information for the first "house" downloads page
+
+house_url = "https://hardwax.com/house/?focus=only_downloads&page=1"
+
+house_request = requests.get(house_url)
+
+soup = BeautifulSoup(house_request.content, 'html.parser')
+
+house_h2_tags = soup.find_all('h2')
+
+house_artist_title = []
+
+for tag in house_h2_tags:
+        house_artist_title.append(tag.get_text())
+
+house_artist_title = list(dict.fromkeys(house_artist_title))
