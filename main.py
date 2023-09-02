4283272186e2a7bbc510f89d32321c8642f3f5a7
iiibrahimaslam423 <iiibrahimaslam@gmail.com>
2023-09-02 09:50:41 -0400

    Add redirect URI variable

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # This main file checks the hardwax scrape results and attempts to add them each to a spotify playlist.
 
-from hardwax import artist_title
+import hardwax
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from api_secrets import CLIENT_ID, CLIENT_SECRET
+
+redirect_uri = 'google.com'
 
 credentials = SpotifyClientCredentials(client_id = CLIENT_ID, client_secret = CLIENT_SECRET)
 
 token = credentials.get_access_token()
 
 spotify = spotipy.Spotify(auth=token)
+
+uris = []
