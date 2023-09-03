5195096750feebb16da4eb7d6932444a857ced2e
iiibrahimaslam423 <iiibrahimaslam@gmail.com>
2023-09-02 21:55:01 -0400

    Begin adding code for creating list of available albums

--- main.py
+++ main.py
@@ -4,16 +4,26 @@ from hardwax import grime_artist_title, house_artist_title
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from api_secrets import CLIENT_ID, CLIENT_SECRET
+import json
 
 redirect_uri = 'https://www.google.com/'
 
 sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=redirect_uri))
 
-grime_json_data = []
-house_json_data = []
+grime_search_results = []
+grime_list = []
 
 for grime_project in grime_artist_title:
-    grime_json_data.append(sp.search(grime_project, 1, 0, 'album', None))
+    grime_search_results.append(sp.search(q = grime_project, limit = 1, type = 'album'))
 
-for house_project in house_artist_title:
-    house_json_data.append(sp.search(house_project, 1, 0, 'album', None))
+# print(grime_search_results[0])
+
+for result in grime_search_results:
+    grime_list.append(result['albums'])
+
+print(grime_list)
+
+
+
+#for json_entry in grime_json_data:
+#    grime_search_results.append()
