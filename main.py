5d2912f8253c464c846793b02d174e939f904b24
iiibrahimaslam423 <iiibrahimaslam@gmail.com>
2023-09-14 20:48:21 -0400

    Remove unused scopes

--- main.py
+++ main.py
@@ -10,25 +10,9 @@ redirect_uri = 'https://www.google.com/'
 
 # I am using all scopes because I am lazy and don't want to read the docs...
 scopes = [
-  'ugc-image-upload',
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'user-read-currently-playing',
-  'streaming',
-  'app-remote-control',
-  'user-read-email',
-  'user-read-private',
-  'playlist-read-collaborative',
   'playlist-modify-public',
   'playlist-read-private',
-  'playlist-modify-private',
-  'user-library-modify',
-  'user-library-read',
-  'user-top-read',
-  'user-read-playback-position',
-  'user-read-recently-played',
-  'user-follow-read',
-  'user-follow-modify'
+  'playlist-modify-private'
 ]
 
 grime_url = 'https://hardwax.com/grime/?focus=only_downloads&page=1'
